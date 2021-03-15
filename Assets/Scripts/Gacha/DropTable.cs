84debee6d247e9fc9dd9df27da2d8d9c053e44e1
Linus Vallinder <divinator.dev@gmail.com>
2021-03-15 18:17:14 +0100

    Basicer UI

--- Assets/Scripts/Gacha/DropTable.cs
+++ Assets/Scripts/Gacha/DropTable.cs
@@ -1,10 +1,8 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
-
 
 [CreateAssetMenu(fileName = "DropTable", menuName = "Drop Table", order = 1)]
 public class DropTable : ScriptableObject
