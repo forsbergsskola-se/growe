849294e3b218f6b8a08ae4fa16eaa64ee93645d3
Kevin Lempa <kevin.p.lempa@gmail.com>
2021-03-15 11:03:09 +0100

    adds Firebase functionailty. (Database, Analytics, Authentication)

---
+++ Assets/Scripts/FireBaseInit.cs
@@ -0,0 +1,25 @@
+using Firebase;
+using Firebase.Analytics;
+using Firebase.Extensions;
+
+using UnityEngine;
+
+
+
+public class FireBaseInit : MonoBehaviour {
+    FireBaseAuthentication auth;
+
+    void Awake() {
+        auth = GetComponent<FireBaseAuthentication>();
+
+
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
+
+            FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+            StartCoroutine(auth.SigninAnonymously());
+        });
+    }
+
+    //WE CAN NOW LOG FIREBASE EVENTS AFTER THIS START METHOD(or inside after set-analytics-collect(true))^^
+ 
+}
