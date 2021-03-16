f0bcda56901925fe36a19d1b4e45df33819d902f
TrushBug <73204786+TrushBug@users.noreply.github.com>
2021-03-16 15:07:32 +0100

    More parameters

--- Assets/Scripts/Inventory/ItemInfoData.cs
+++ Assets/Scripts/Inventory/ItemInfoData.cs
@@ -5,7 +5,7 @@ namespace Inventory {
     public class ItemInfoData : MonoBehaviour
     {
         public Image itemIcon;
-        public Text itemName, itemAmount, itemParameters, itemLore;
+        public Text itemName, itemAmount, itemRarity, itemSize, itemSurvivability, itemCompostValue, itemSellValue, itemLore;
         public ItemData itemData;
         
 
@@ -14,8 +14,13 @@ namespace Inventory {
             var itemInfo = itemData.itemSo;
             itemName.text = itemInfo.name;
             itemIcon.sprite = itemInfo.icon;
+            itemRarity.text = itemInfo.rarity.ToString();
             itemAmount.text = itemData.amount.ToString();
-            itemParameters.text = $"{itemInfo.rarity} {itemInfo.itemType}";
+            itemSize.text = $"{itemInfo.sizeDimensions.x} x {itemInfo.sizeDimensions.y}";
+            itemSurvivability.text = itemInfo.survivability.ToString();
+            itemCompostValue.text = itemInfo.compostValue.ToString();
+            itemSellValue.text = itemInfo.sellValue.ToString();
+
             itemLore.text = itemInfo.itemLore;
         }
     }
