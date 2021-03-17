261ed07ac288e99aad1aad4f7c189414f6cfeec8
Ruben Rådelius <ruben.radelius@gmail.com>
2021-03-17 11:36:51 +0100

    adds more public settings fields and clamps AddCompost amount

--- Assets/CompostTracker.cs
+++ Assets/CompostTracker.cs
@@ -1,23 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class CompostTracker : MonoBehaviour {
     public Slider leftSlider;
     public Slider rightSlider;
     public int maxValue = 15;
+    public int maxAddAmount = 3;
+    public int minAddAmount = 2;
     int _compostValue;
 
-    public void AddCompost(int amount) {
-        _compostValue += amount;
+    void Start() {
+        leftSlider.minValue = 0;
+        rightSlider.minValue = 0;
+        leftSlider.maxValue = maxValue;
+        rightSlider.maxValue = maxValue;
+    }
+
+    public void AddCompost(int compostAmount) {
+        compostAmount = Mathf.Clamp(compostAmount, minAddAmount, maxAddAmount);
+        _compostValue += compostAmount;
         
         if (_compostValue > maxValue) {
             var overflow = _compostValue - maxValue;
             _compostValue = overflow;
+            
+            //TODO add fertilizer event or something + animation
         }
-        
         UpdateSliders();
     }
 
