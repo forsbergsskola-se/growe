d461d267cb0b5be9b64c49996f4fe904a8558830
dafors <da_fors@hotmail.com>
2021-03-19 11:15:30 +0100

    Make plant spawn in the middle of the grid.

--- Assets/Scripts/PlantSpawner.cs
+++ Assets/Scripts/PlantSpawner.cs
@@ -5,12 +5,12 @@ public class PlantSpawner : MonoBehaviour
 {
     public GameObject pottedPlantPlaceholder;  // replace with a gridobject of type potted plant
     public Vector3 startOffsetFromGrid;
-    private Transform gridTransform;
+    private Grid grid;
 
     private void OnEnable()
     {
-        gridTransform = FindObjectOfType<Grid>().gameObject.transform;
-        if (gridTransform == null)
+        grid = FindObjectOfType<Grid>();
+        if (grid == null)
             Debug.LogWarning("grid not found", this);
     }
 
@@ -18,8 +18,11 @@ public class PlantSpawner : MonoBehaviour
     public void SpawnPlant(Item item)
     {
         // TODO add a check that the item is a potted plant or change the parameter type.
-        GameObject instance = Instantiate(pottedPlantPlaceholder, gridTransform.position + startOffsetFromGrid, gridTransform.rotation, gridTransform );
-        //instance.GetComponent<GridObject>();
+        GameObject instance = Instantiate(pottedPlantPlaceholder, grid.transform.position + startOffsetFromGrid, grid.transform.rotation, grid.transform );
+        Vector3Int pos = new Vector3Int();
+        pos.x = (int) (grid.width * 0.5);
+        pos.y = (int) (grid.height * 0.5);
+        instance.transform.localPosition = pos;
         // TODO pass Item reference to instance
     }
 
