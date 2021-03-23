0ffacfb94bb06d5c2c26061a3abf38902aaa9ab8
Ruben Rådelius <ruben.radelius@gmail.com>
2021-03-23 15:28:31 +0100

    adds logic to AddCompost in Currency

--- Assets/Scripts/UI/CompostTracker.cs
+++ Assets/Scripts/UI/CompostTracker.cs
@@ -1,3 +1,5 @@
+using Broker;
+using Broker.Messages;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,41 +7,35 @@ namespace UI {
     public class CompostTracker : MonoBehaviour {
         public Slider leftSlider;
         public Slider rightSlider;
-        public int maxValue = 15;
-        public int maxAddAmount = 3;
-        public int minAddAmount = 2;
-        int _compostValue;
+        int _compostAmount;
 
         void Start() {
             leftSlider.minValue = 0;
             rightSlider.minValue = 0;
-            leftSlider.maxValue = maxValue;
-            rightSlider.maxValue = maxValue;
+            leftSlider.maxValue = 15;
+            rightSlider.maxValue = 15;
+            //TODO get maxCompostValue from Currency
+        }
+
+        void OnEnable() {
+            MessageBroker.Instance().SubscribeTo<CompostUpdateMessage>(UpdateCompostAmount);
+        }
+
+        void OnDisable() {
+            MessageBroker.Instance().UnSubscribeFrom<CompostUpdateMessage>(UpdateCompostAmount);
+        }
         
-            //TODO set _compostValue value to saved compost data
+        void UpdateCompostAmount(CompostUpdateMessage m) {
+            _compostAmount = m.amount;
             UpdateSliders();
         }
 
-        
-        //TODO the logic in this method should be moved to Currency
-        //TODO This method should only read a value from CompostUpdateMessage and display it
-        public void AddCompost(int compostAmount) {
-            compostAmount = Mathf.Clamp(compostAmount, minAddAmount, maxAddAmount);
-            _compostValue += compostAmount;
-        
-            if (_compostValue > maxValue) {
-                var overflow = _compostValue - maxValue;
-                _compostValue = overflow;
-            
-                //TODO add event that tells listeners to increase fertilizer by 1
-                //TODO OR use FindObjectOfType -> GameManager -> GetComponent -> Currency and use AddFertilizer(1) method
-            }
-            UpdateSliders();
-        }
+        //TODO add event that tells listeners to increase fertilizer by 1
+        //TODO OR use FindObjectOfType -> GameManager -> GetComponent -> Currency and use AddFertilizer(1) method
 
         void UpdateSliders() {
-            leftSlider.value = _compostValue;
-            rightSlider.value = _compostValue;
+            leftSlider.value = _compostAmount;
+            rightSlider.value = _compostAmount;
         }
     }
 }
