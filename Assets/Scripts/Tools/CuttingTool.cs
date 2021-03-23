be64fd80de317140164d7ade2039b5a38e4db571
Bulteze <48334398+Bulteze@users.noreply.github.com>
2021-03-23 14:09:37 +0100

    Added Cutting Tool logics, replace later with plant data instead of local data.

--- Assets/Scripts/Tools/CuttingTool.cs
+++ Assets/Scripts/Tools/CuttingTool.cs
@@ -1,10 +1,14 @@
 using System;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
+using Random = UnityEngine.Random;
 
 public class CuttingTool : MonoBehaviour {
     [HideInInspector] public bool isCutting;
     
+    //Temporary values
+    int growthStage = 5, survivability = 50, timesCutten = 0;
+
     public void Cancel() {
         //TODO: Add cancel button.
         //Should only set isCutting to false, should be called from the CancelBtn later.....       
@@ -23,16 +27,22 @@ public class CuttingTool : MonoBehaviour {
         if (!Input.GetMouseButtonDown(0)) return;
         //Debug.Log("Pressed mouse 0");
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        if (Physics.Raycast(ray, out var hit) && growthStage >= 4) {
+            //TODO WHEN ASSET IS DONE: Add a filter so we can only press the plant ALSO replace temp variables with the real ones:
 
-        if (Physics.Raycast(ray, out var hit)) {
-            //Debug.Log(hit.collider.gameObject.name);
-            //Debug.Log("did hit");
-                    
-            //TODO: REPLACE ABOVE WITH:
-            //If we hit plant, getcomponent<SO>() and check for GrowthStage
-            //
-            //Chance for plant to die when cut.
-            //Add a count to the Plant, if the count reaches 4 cuts, the plant die.
+            var shouldDie = Random.Range(0, 100);
+            if (shouldDie >= survivability || timesCutten >= 4) {
+                //Kill Plant
+                Debug.Log($"Plant died : Rolled {shouldDie} out of 100 , Plant was cutten {timesCutten} times.");
+            }
+            else {
+                growthStage = 3;
+                timesCutten += 1;
+                //TODO: Add cuttings to inventory here.
+                
+                Debug.Log($"Plant succesfully cutted, current Growth Stage = {growthStage} and Times plant was cutted = {timesCutten}");
+            }
         }
         isCutting = false;
     }
