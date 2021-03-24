8794c5e5e5f29502ba6d793175ecd585d57acccb
Liam <liamthynne@gmail.com>
2021-03-24 13:15:41 +0100

    removed weird looking stuf

--- Assets/Scripts/InventoryAndStore/Currency.cs
+++ Assets/Scripts/InventoryAndStore/Currency.cs
@@ -16,10 +16,6 @@ namespace InventoryAndStore {
         private CurrencyData _data;
         private InventoryData _inventoryData;
         private AuctionData _auctionData;
-<<<<<<< Updated upstream
-=======
-        private AuctionData _auctionData;
->>>>>>> main
         private SaveManager _saveManager;
         private bool _hasLoaded;
 
@@ -45,20 +41,21 @@ namespace InventoryAndStore {
                 MessageBroker.Instance().Send(new InventoryUpdateMessage(_inventoryData.Inventory));
                 MessageBroker.Instance().Send(new FertilizerUpdateMessage(_data.Fertilizer));
                 MessageBroker.Instance().Send(new CompostUpdateMessage(_data.Compost));
-<<<<<<< Updated upstream
+                
+                /* Todo FIX
                 MessageBroker.Instance().Send(new SoftCurrencyUpdateMessage(_data.softCurrency));
                 MessageBroker.Instance().Send(new FertilizerUpdateMessage(_data.fertilizer));
                 MessageBroker.Instance().Send(new CompostUpdateMessage(_data.compost));
+                */
             }
             else
             {
                 Debug.LogWarning("Couldn't load data" + this);
-=======
+                
                 MessageBroker.Instance().Send(new SoftCurrencyUpdateMessage(_data.SoftCurrency));
                 MessageBroker.Instance().Send(new AuctionUpdateMessage(_auctionData.Item));
                 MessageBroker.Instance().Send(new FertilizerUpdateMessage(_data.Fertilizer));
                 MessageBroker.Instance().Send(new CompostUpdateMessage(_data.Compost));
->>>>>>> main
             }
         }
 
@@ -71,7 +68,7 @@ namespace InventoryAndStore {
         public void AddItemForAuction(ItemSO item) {
             if (!_hasLoaded) return;
             _auctionData.Item += JsonConvert.SerializeObject(ConvertSO.SOToClass(item), Formatting.Indented);;
-            _saveManager.UploadToAuction(_auctionData);
+            //_saveManager.UploadToAuction(_auctionData);
             MessageBroker.Instance().Send(new AuctionUpdateMessage(_auctionData.Item));
         }
         public void FireBaseSetUserInventory(Inventory inventory) {
