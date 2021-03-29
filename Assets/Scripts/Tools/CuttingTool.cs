9831e72e6a9a4282de41b712ea8eb5c8aef7ba9c
Virginia Baroni <virg.baroni@gmail.com>
2021-03-29 16:47:56 +0200

    cutting tool logic is done - missing sprites

--- Assets/Scripts/Tools/CuttingTool.cs
+++ Assets/Scripts/Tools/CuttingTool.cs
@@ -31,47 +31,50 @@ public class CuttingTool : MonoBehaviour {
         if (!isCutting) return;
         if (!Input.GetMouseButton(0)) return;
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        var chance = chanceOfDeath / 100;
+        var chance = chanceOfDeath / 100f;
         
         if (Physics.Raycast(ray, out var hit)){
             //loop currently gets here
             if (hit.collider.transform.GetChild(2).GetComponent<GridPlant>() != null) {
                 var currentPlant = hit.collider.transform.GetChild(2).GetComponent<GridPlant>();
+                Debug.Log((currentPlant.plant.CurrentGrowthStage));
                 //if (currentPlant.plant.growthStage <= 3) return;
-                if (currentPlant.plant.CurrentGrowthStage != ItemSO.GrowthStage.Growing ||
-                    currentPlant.plant.CurrentGrowthStage != ItemSO.GrowthStage.Mature)
-                    return;
-                
-                var timesCut = currentPlant.plant.timesCut;
-                var canDie = currentPlant.plant.survivability * chance;
-                Debug.Log(canDie);
+                if (currentPlant.plant.CurrentGrowthStage == ItemSO.GrowthStage.Growing || 
+                    currentPlant.plant.CurrentGrowthStage == ItemSO.GrowthStage.Mature)
+                {
+                    var timesCut = currentPlant.plant.timesCut;
+                    var canDie = currentPlant.plant.survivability * chance;
+                    Debug.Log(currentPlant.plant.survivability);
+                    Debug.Log(canDie);
 
-                var range = Random.Range(0, 100);
-                if (range <= canDie || timesCut >= 4) {
-                    Destroy(currentPlant.transform.parent.gameObject);
+                    var range = Random.Range(0, canDie);
+                    Debug.Log(range);
+                    if (range <= canDie || timesCut >= 4) {
+                        Destroy(currentPlant.transform.parent.gameObject);
+                    }
+                    else {
+                        //currentPlant.plant.growthStage = 3;
+                        currentPlant.plant.CurrentGrowthStage = ItemSO.GrowthStage.Sapling;
+                        currentPlant.plant.timesCut += 1;
+                    }
+
+                    //TODO: Add cuttings to inventory here.
+                    isCutting = false;
+                    ItemSO newCutting = Instantiate(currentPlant.plant);
+                    newCutting.itemType = ItemSO.ItemType.Cutting;
+                    newCutting.timesCut = 0;
+                    newCutting.CurrentGrowthStage = ItemSO.GrowthStage.Cutting;
+                    newCutting.survivability = 0;
+                    newCutting.hasLifeTime = true;
+
+                    //newCutting.Icon = cutting icon (?)
+                
+                    Inventories.Instance.playerInventory.Add(newCutting);
+                
+                    // Debug.Log(timesCut);
+                    // Debug.Log(currentPlant.item.growthStage);
+                    // currentPlant.item.growthStage = 5;
                 }
-                else {
-                    //currentPlant.plant.growthStage = 3;
-                    currentPlant.plant.CurrentGrowthStage = ItemSO.GrowthStage.Sapling;
-                    currentPlant.plant.timesCut += 1;
-                }
-
-                //TODO: Add cuttings to inventory here.
-                isCutting = false;
-                ItemSO newCutting = Instantiate(currentPlant.plant);
-                newCutting.itemType = ItemSO.ItemType.Cutting;
-                newCutting.timesCut = 0;
-                newCutting.CurrentGrowthStage = ItemSO.GrowthStage.Cutting;
-                newCutting.survivability = 0;
-                newCutting.hasLifeTime = true;
-
-                //newCutting.Icon = cutting icon (?)
-                
-                Inventories.Instance.playerInventory.Add(newCutting);
-                
-                // Debug.Log(timesCut);
-                // Debug.Log(currentPlant.item.growthStage);
-                // currentPlant.item.growthStage = 5;
             }
         }
     }
