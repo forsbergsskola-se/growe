a55f8e1c024de5094387fc0d979c27184aa023a6
Kevin Lempa <kevin.p.lempa@gmail.com>
2021-03-31 22:35:38 +0200

    no message

--- Assets/Scripts/UI/OpenURL.cs
+++ Assets/Scripts/UI/OpenURL.cs
@@ -1,14 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class OpenURL : MonoBehaviour
 {
-    public string Url;
 
 
     public void Open()
     {
-        Application.OpenURL(Url);
+        Debug.Log("Open");
+        Application.OpenURL(Application.persistentDataPath+ "/index.html");
     }
 }
