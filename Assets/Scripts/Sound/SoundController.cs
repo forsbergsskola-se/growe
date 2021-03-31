d41889b1cfdfef092a0e0d005d6a207dfdcf9325
Virginia Baroni <virg.baroni@gmail.com>
2021-03-31 14:38:59 +0200

    Sound prefab is done - waiting for sliders to trigger mute.

--- Assets/Scripts/Sound/SoundController.cs
+++ Assets/Scripts/Sound/SoundController.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SoundController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string volumeParameter;
+    public AudioMixer audioMixer;
+    
+    void Awake()
     {
-        
+        GetVolume(audioMixer, volumeParameter);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //if sfx slider is off
+        //SetVolume(audioMixer, volumeParameter, 0.0f);
+        //if music slider is off
+        //SetVolume(audioMixer, volumeParameter, 0.0f);
+    }
+    
+    public static void SetVolume(AudioMixer mixer, string exposedName, float value)
+    {
+        mixer.SetFloat(exposedName, Mathf.Lerp(-80.0f, 0.0f, Mathf.Clamp01(value)));
+    }
+    
+    public static float GetVolume(AudioMixer mixer, string exposedName)
+    {
+        if (mixer.GetFloat(exposedName, out float volume))
+        {
+            return Mathf.InverseLerp(-80.0f, 0.0f, volume);
+        }
+ 
+        return 0f;
     }
 }
