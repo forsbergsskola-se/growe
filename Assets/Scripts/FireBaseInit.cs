938e2817bf6b57d80f35a407d4e3c0b8b4c68383
Kevin Lempa <kevin.p.lempa@gmail.com>
2021-03-31 12:00:08 +0200

    removes login screen, anonymous login as default.

--- Assets/Scripts/FireBaseInit.cs
+++ Assets/Scripts/FireBaseInit.cs
@@ -1,22 +1,26 @@
 using Firebase;
 using Firebase.Analytics;
+using Firebase.Auth;
 using Firebase.Extensions;
 using UnityEngine;
 
 public class FireBaseInit : MonoBehaviour {
     FireBaseAnonymousAuthentication authAnonymous;
+    FirebaseAuth _auth;
     
     void Awake() {  // FirebaseAuthentication was initialized
         authAnonymous = GetComponent<FireBaseAnonymousAuthentication>();
-        
+        AnonymousLogIn();
+
     }
     public void AnonymousLogIn()
     {
         FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task => {
 
             FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+            if (_auth.CurrentUser != null) return;
             StartCoroutine(authAnonymous.SigninAnonymously());
         });
     }
-    //WE CAN NOW LOG FIREBASE EVENTS AFTER THIS START METHOD(or inside after set-analytics-collect(true))^^
+    
 }
